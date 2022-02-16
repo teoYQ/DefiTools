655d66163775c6861d42d03b161743989a927f6d
yq <yongquan_teo@mymail.sutd.edu.sg>
2022-02-16 22:12:42 +0800

    Update readme and add tombfork.js

--- README.md
+++ README.md
@@ -20,3 +20,26 @@ This works for the nuclues farm $PROTO - $FTM LP and stakes the electrons earned
 You can easily modify this by changing the farm id to work for $PROTO - $USDT instead in line 15. set `farm id = 2`<br>
 The code can be easily modified to make it work for fusion farms, harvest $PROTO, stake in Particle farm. Or if you're more adept at coding, then you could do a harvest $PROTO, sell half of the $PROTO and form a $PROTO - $FTM LP, stake the LP and harvest $ELCT and stake in fission for the maximum APY!<br>
 Do Reach out if you have any questions!
+
+
+####  Tomb & Tombforks autocompounder
+
+- Requirements (Same as above)
+    - Node (https://nodejs.org/en/download/)
+    - Run the following command in terminal (npm install web3) 
+    - Metamask Wallet (https://metamask.io/download/)
+    - Metamask private key (https://metamask.zendesk.com/hc/en-us/articles/360015289632-How-to-Export-an-Account-Private-Key)
+- How to Run
+    1) Clone the repository
+    2) In line 3 of TombFork.js, insert ur private key 
+        `const wallet = web3.eth.accounts.wallet.add('INSERT YOUR PRIVATE KEY HERE')`
+    3)  In line 91 of TombFork.js set your polling interval ( defualt is at 6hours) aka how often to compound
+    `const POLLING_INTERVAL = 21600000 // 1000 = 1s , 60000 = 1 min`
+    4) Save the file, launch your terminal and run 
+    `node TombFork.js`   
+
+##### Additional info
+The sample code already works for ScarabFinance and should work for Tomb and most tomb forks ( unless their contracts are not verified, then you cant get the contract ABI. This code automatically harvest $TSHARE from ur tomb-ftm LP (gscarab and scarab-ftm LP respectively for scarabfinance) and deposits it in the Masonry (temple for scarab finance) <br>
+You can easily modify this by changing the pid to work for $TSHARE - $FTM instead in line 12. set `pid = 1`<br>
+Will be upgrading this automation tool to sell 50% of the $TOMB collected from the masonry into $FTM to form the TOMB - FTM LP and deposit it back into the Cemetry<br>
+Do Reach out if you have any questions!
---
+++ TombFork.js
@@ -0,0 +1,87 @@
+const Web3 = require('web3')
+const web3 = new Web3('https://rpc.ftm.tools/')
+const wallet = web3.eth.accounts.wallet.add('INSERT PRIVATE KEY HERE')
+//or use an .env file if you wish
+//require('dotenv').config()
+//const wallet = web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY) 
+
+//constants
+const gasLimit = 300000
+const TombForkAddress = '0xc88690163b10521d5fB86c2ECB293261F7771525'
+const TombForkAbi =[{"inputs":[{"internalType":"address","name":"_tshare","type":"address"},{"internalType":"uint256","name":"_poolStartTime","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"EmergencyWithdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"RewardPaid","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdraw","type":"event"},{"inputs":[],"name":"TOTAL_REWARDS","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_allocPoint","type":"uint256"},{"internalType":"contract IERC20","name":"_token","type":"address"},{"internalType":"bool","name":"_withUpdate","type":"bool"},{"internalType":"uint256","name":"_lastRewardTime","type":"uint256"}],"name":"add","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"emergencyWithdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_fromTime","type":"uint256"},{"internalType":"uint256","name":"_toTime","type":"uint256"}],"name":"getGeneratedReward","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"contract IERC20","name":"_token","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"address","name":"to","type":"address"}],"name":"governanceRecoverUnsupported","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"massUpdatePools","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"operator","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"address","name":"_user","type":"address"}],"name":"pendingShare","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"poolEndTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"poolInfo","outputs":[{"internalType":"contract IERC20","name":"token","type":"address"},{"internalType":"uint256","name":"allocPoint","type":"uint256"},{"internalType":"uint256","name":"lastRewardTime","type":"uint256"},{"internalType":"uint256","name":"accTSharePerShare","type":"uint256"},{"internalType":"bool","name":"isStarted","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"poolStartTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"runningTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_allocPoint","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_operator","type":"address"}],"name":"setOperator","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"tSharePerSecond","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalAllocPoint","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tshare","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"updatePool","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"address","name":"","type":"address"}],"name":"userInfo","outputs":[{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"rewardDebt","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+const pid = 0
+const TombFork = new web3.eth.Contract(TombForkAbi,TombForkAddress)
+const MasonryAddress = '0xD00F41d49900d6affd707EC6a30d1Bf7D4B7dE8F'
+const MasonryABI = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"executor","type":"address"},{"indexed":false,"internalType":"uint256","name":"at","type":"uint256"}],"name":"Initialized","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"reward","type":"uint256"}],"name":"RewardAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"reward","type":"uint256"}],"name":"RewardPaid","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Staked","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdrawn","type":"event"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"allocateSeigniorage","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"mason","type":"address"}],"name":"canClaimReward","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"mason","type":"address"}],"name":"canWithdraw","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"claimReward","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"mason","type":"address"}],"name":"earned","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"epoch","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"exit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"mason","type":"address"}],"name":"getLastSnapshotIndexOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getTombPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"contract IERC20","name":"_token","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"},{"internalType":"address","name":"_to","type":"address"}],"name":"governanceRecoverUnsupported","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract IERC20","name":"_tomb","type":"address"},{"internalType":"contract IERC20","name":"_share","type":"address"},{"internalType":"contract ITreasury","name":"_treasury","type":"address"}],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"initialized","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestSnapshotIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"masonryHistory","outputs":[{"internalType":"uint256","name":"time","type":"uint256"},{"internalType":"uint256","name":"rewardReceived","type":"uint256"},{"internalType":"uint256","name":"rewardPerShare","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"masons","outputs":[{"internalType":"uint256","name":"lastSnapshotIndex","type":"uint256"},{"internalType":"uint256","name":"rewardEarned","type":"uint256"},{"internalType":"uint256","name":"epochTimerStart","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nextEpochPoint","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"operator","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rewardLockupEpochs","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rewardPerShare","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_withdrawLockupEpochs","type":"uint256"},{"internalType":"uint256","name":"_rewardLockupEpochs","type":"uint256"}],"name":"setLockUp","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_operator","type":"address"}],"name":"setOperator","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"share","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"stake","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"tomb","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"treasury","outputs":[{"internalType":"contract ITreasury","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawLockupEpochs","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
+const Masonry = new web3.eth.Contract(MasonryABI,MasonryAddress)
+
+//const FissionContract = new web3.eth.Contract(FissionAbi,FissionAddress)
+const ShareAddress = '0x6ab5660f0b1f174cfa84e9977c15645e4848f5d6'
+
+
+async function checkHarvestBalance(pid){
+    const pendingRewards = await TombFork.methods.pendingShare(pid,wallet.address).call()
+    console.log('Current Share Rewards : ')
+    console.log(web3.utils.fromWei(pendingRewards.toString(),'ether'))
+    return pendingRewards
+}
+
+
+//function to harvest rewards from 
+async function Harvest(id){
+    const gasPrice = await web3.eth.getGasPrice()
+    try{
+        const withdrawTx = await TombFork.methods.withdraw(id,0).send(
+            {
+                from : wallet.address,
+                gas: gasLimit,
+                gasPrice: gasPrice 
+            }
+
+        ) 
+        console.log(`withdraw status: ${withdrawTx.status}`)
+        //console.log(`Withdrew ${web3.utils.fromWei(curBal.toString(),'ether')}`)
+
+    }
+    catch (err){
+        console.log(`Withdraw Ball error ${err.message}`)
+    }
+}
+
+
+async function Deposit(share){
+    
+    try{
+        const gasPrice = await web3.eth.getGasPrice()
+
+        const depositTx = await Masonry.methods.stake(share).send(
+            {
+                from : wallet.address,
+                gas: gasLimit,
+                gasPrice: gasPrice 
+                
+            }
+
+        )
+        console.log(`deposit status: ${depositTx.status}`)
+        console.log(`Deposited ${web3.utils.fromWei(share.toString(),'ether')}`)
+
+    }
+    catch (err){
+        console.log(`Deposit share error ${err.message}`)
+    }
+}
+
+async function AutoCompound(){
+    const contractBal = await checkHarvestBalance(pid)
+    const gasPrice = await web3.eth.getGasPrice() 
+    const txCost = web3.utils.fromWei(gasPrice.toString(),'ether') * gasLimit 
+    console.log("Current Balance, GasPrice, Transaction Cost")
+    await Harvest(pid);
+    console.log(web3.utils.fromWei(contractBal.toString(),'ether'),gasPrice,txCost)
+    await Deposit(contractBal)
+}
+
+
+const POLLING_INTERVAL = 21600000 // 1000 = 1s , 60000 = 1 min
+setInterval(async () => { AutoCompound()}, POLLING_INTERVAL)
